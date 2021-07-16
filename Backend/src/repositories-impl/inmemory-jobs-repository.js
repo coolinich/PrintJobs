b2fecd158542a3fc5d643aacbcdad9abb1a5ac66
coolinich <coolinich@gmail.com>
2021-07-16 11:41:34 +0300

    BE refactoring: separated business logic

---
+++ Backend/src/repositories-impl/inmemory-jobs-repository.js
@@ -0,0 +1,13 @@
+export default class InMemoryJobsRepository {
+    constructor() {
+        this.jobs = [];
+    }
+
+    async addPrintJob(job) {
+        this.jobs.push(job);
+    }
+
+    async getAllPrintJobs() {
+        return this.jobs;
+    }
+};
