d4fd8644abe3544cb6c689d279c9682c23df0307
coolinich <coolinich@gmail.com>
2021-07-19 11:17:55 +0300

    added component to switch between languages

---
+++ Frontend/src/app/components/shared/switch-languages/switch-languages.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SwitchLanguagesComponent } from './switch-languages.component';
+
+describe('SwitchLanguagesComponent', () => {
+  let component: SwitchLanguagesComponent;
+  let fixture: ComponentFixture<SwitchLanguagesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SwitchLanguagesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SwitchLanguagesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
